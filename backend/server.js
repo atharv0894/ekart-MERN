2f2e741deb964e50d9f08189d2729ae25fa643c3
atharv0894 <shindeatharv36@gmail.com>
2026-04-21 20:18:04 +0530

    fix:cors

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-aapp.use(cors({
+app.use(cors({
   origin: "https://ekart-frontend-theta.vercel.app",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
